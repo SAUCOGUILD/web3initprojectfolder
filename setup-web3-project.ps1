9dd6d90a639a7d2c3d8a653b9cf0ab65e667cff7
SAUCOGUILD <156845927+SAUCOGUILD@users.noreply.github.com>
2024-01-20 15:05:34 +0100

    Add files via upload
    script para windows iniciar proyect web3
    1creas una carpeta que se llame "mi-projecto-web3"
    2 ejecutas en la powershell el script aka .\setup-web3-project.ps1
    y ya esta

---
+++ setup-web3-project.ps1
@@ -0,0 +1,27 @@
+# Definir el nombre del proyecto
+$PROJECT_NAME = "mi-proyecto-web3"
+
+# Crear el directorio del proyecto y navegar a él
+New-Item -ItemType Directory -Path $PROJECT_NAME
+Set-Location $PROJECT_NAME
+
+# Inicializar un proyecto Node.js
+npm init -y
+
+# Inicializar proyecto Truffle
+truffle init
+
+# Crear directorios para el cliente (React) y el servidor (Node.js)
+New-Item -ItemType Directory -Path "client", "server"
+
+# Inicializar la aplicación React en el directorio del cliente
+Set-Location client
+npx create-react-app .
+Set-Location ..
+
+# Inicializar un proyecto Node.js para el servidor
+Set-Location server
+npm init -y
+Set-Location ..
+
+Write-Host "Estructura del proyecto Web3 creada con éxito."
